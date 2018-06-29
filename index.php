bbce7fdf60d83eb4947caff471b8470ffa355563
hou <hou@sino.com>
2018-06-29 15:47:00 +0800

    first

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+class Demo1Controller extends Controller {
+    public function index(){
+            echo 'hello world-111';
+        }
+}
